9f269c8c73f35d980b02d757e21f404c45f16b11
leonardomunoz <l.mnz.lpz@gmail.com>
2023-05-23 20:10:34 -0600

    Created sphere

--- main.js
+++ main.js
@@ -14,6 +14,14 @@ renderer.setSize(innerWidth,innerHeight)
 
 document.body.appendChild(renderer.domElement)
 
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(5,50,50), new THREE.MeshBasicMaterial({
+    color: 0xFF000
+}))
+
+scene.add(sphere)
+
+camera.position.z = 6
+
 function animate(){
     requestAnimationFrame(animate)
     renderer.render(scene, camera)
